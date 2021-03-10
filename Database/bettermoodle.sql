ca53c713682e284a270662a9b0b7a59c563b09d6
Jamie <jh2883@bath.ac.uk>
2021-03-10 11:03:40 +0000

    Added Bath as institution for institution table

--- Database/bettermoodle.sql
+++ Database/bettermoodle.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 02, 2021 at 11:57 AM
+-- Generation Time: Mar 10, 2021 at 11:49 AM
 -- Server version: 10.4.17-MariaDB
--- PHP Version: 7.3.27
+-- PHP Version: 8.0.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -43,6 +43,13 @@ CREATE TABLE `institution` (
   `InstitutionID` int(11) NOT NULL,
   `Name` varchar(32) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `institution`
+--
+
+INSERT INTO `institution` (`InstitutionID`, `Name`) VALUES
+(0, 'Bath');
 
 -- --------------------------------------------------------
 
